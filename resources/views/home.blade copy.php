e99cfa05c6d949855c7519003cefc36ab0727677
ss <evo0622@gmail.com>
2023-01-06 01:02:44 +0900

    home list

---
+++ resources/views/home.blade copy.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+        <!-- <div class="container py-5">
+            <div id="example"></div>
+        </div> -->
+        <!-- @isset($meetings)
+            <div id="calendar" data-calendar-meetings="{{ $meetings }}" data-user="{{ json_encode(Auth::user()->name) }}"></div>
+        @endisset -->
+
+        @isset($stocks)
+        <div id="stock" data-stocks='{{ $stocks }}'></div>
+        @endisset
+
+    </div>
+</div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,38 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+<div class="p-card">
+        <div class="c-flexbox--index">
+            <div class="c-flexbox__flexcontainer c-flexbox__flexcontainer--index">
+                @foreach ($stocks as $stock)
+                    <div class="c-flexbox__flexitem c-flexbox__flexitem--index">
+                        <div class="p-card p-card__header--index u-overflow">{{$stock->name}}</div>
                     </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                @endforeach
             </div>
-        </div>
-        <div class="container py-5">
-            <div id="example"></div>
-        </div>
-        <!-- @isset($meetings)
-            <div id="calendar" data-calendar-meetings="{{ $meetings }}" data-user="{{ json_encode(Auth::user()->name) }}"></div>
-        @endisset -->
-
-        @isset($stocks)
-        <div id="stock" data-stocks='{{ $stocks }}'></div>
-        @endisset
-
-        
-    </div>
-   
-</div>
+        </div>    
+    </div> 
 
 <script src="{{ asset('js/app.js') }}"></script>
 @endsection
