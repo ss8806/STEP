1f57ad63bf40568c0f5eb2698c5a5bc4e160a1cc
ss <evo0622@gmail.com>
2023-01-14 15:40:25 +0900

    email profile

--- app/Http/Requests/ProfileRequest.php
+++ app/Http/Requests/ProfileRequest.php
@@ -24,7 +24,7 @@ class ProfileRequest extends FormRequest
     public function rules()
     {
         return [
-            'editEmail'   => 'max:10',
+            'editEmail'   => 'max:10|email',
         ];
     }
 
