6e675ca712c4e1f25e74e9aa88ee77eefade580a
ss <evo0622@gmail.com>
2023-03-12 23:57:05 +0900

    top2

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,20 +5,19 @@ TOP
 @endsection
 
 @section('content')
-<header>
-    <h1 id="logo">
-        <a href="index.html"><img src="images/logo.png" alt="SAMPLE COMPANY" /></a>
-    </h1>
-    <nav id="menubar">
-        <ul>
-            <li><a href="#service">Service</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
-    </nav>
+
+<header class="p-top__header">
+    <img class="p-top__logo" src="images/logo.png" alt="SAMPLE COMPANY" />
+    <ul class="p-top__menubar">
+        <li><a href="{{route('register')}}" class="c-link__signup">新規会員登録</a></li>
+        <li><a href="{{route('login')}}" class="c-link__login">ログイン</a></li>
+    </ul>
 </header>
-<footer>
+
+
+<!-- <footer>
     <small>Copyright&copy; <a href="index.html">SAMPLE COMPANY</a> All Rights
         Reserved.</small>
     <span class="pr">《<a href="https://template-party.com/" target="_blank">Web Design:Template-Party</a>》</span>
-</footer>
+</footer> -->
 @endsection
