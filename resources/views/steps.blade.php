ed7eac1687ce0d9863cd2b01dee09f43c627edef
ss <evo0622@gmail.com>
2023-03-27 00:09:49 +0900

    pager

--- resources/views/steps.blade.php
+++ resources/views/steps.blade.php
@@ -8,4 +8,10 @@
 @endisset
 
 <script src="{{ asset('js/app.js') }}"></script>
+
+
+<!-- ページャー  検索結果を保持したままページネーション  -->
+<div class="c-pager">
+  {{ $steps->appends(request()->input())->links() }}
+</div>
 @endsection
