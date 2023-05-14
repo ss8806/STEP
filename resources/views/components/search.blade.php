efb23b47495df881fe8e31a175827f300d6fefdd
ss <evo0622@gmail.com>
2023-05-14 21:13:05 +0900

    search

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -2,8 +2,8 @@
 <div class="p-search">
     <form class="p-form" method="GET" action="{{ route('steps') }}">
         <div id="search"
-        data-aboveday="{{ json_encode($defaults['aboveday']) ?? ''  }}"
-        data-belowday="{{ json_encode($defaults['belowday']) ?? '' }}"
+        data-aboveday="{{ json_encode(['aboveday']) ?? ''  }}"
+        data-belowday="{{ json_encode(['belowday']) ?? '' }}"
         >
         </div>
     </form>
@@ -13,6 +13,7 @@
         <strong>{{ $message }}</strong>
     </div>
     @enderror
+
     @error('belowday')
     <div class="c-error" role="alert">
         <strong>{{ $message }}</strong>
